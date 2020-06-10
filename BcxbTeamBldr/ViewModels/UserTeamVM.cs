147f49f1416dea8b24a26be6e3dc5e1a74085839
bobc701 <bobc@4bcx.com>
2020-06-09 22:16:05 -0400

    Team ready logic in process

--- BcxbTeamBldr/ViewModels/UserTeamVM.cs
+++ BcxbTeamBldr/ViewModels/UserTeamVM.cs
@@ -43,7 +43,7 @@ namespace BcxbTeamBldr.ViewModels {
       }
 
 
-      public bool Ready {
+      public string Ready {
 
          /* ------------------------------------------------------------
           * Rules:
@@ -54,25 +54,56 @@ namespace BcxbTeamBldr.ViewModels {
           * Posn_NoDH must have 1..9, same as Slot_DH
           * Slot_DH must have 1..9
           * Posn_DH must have 1..10, 2 to 9 must match Slot_DH, 1 must not bat, 10 must not field
+          * -----------------------------------------------------------
+          * Return: "" if ok, else message
           * -----------------------------------------------------------*/
-
          get {
+
             int totB = Players.Count(p => p.PlayerType == "B");
             int totP = Players.Count(p => p.PlayerType == "P");
             int totAll = totB + totP;
             string msg = "";
 
-            //var slot = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            //int[] posn = new int[10] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            //foreach (var p in Players) {
-            //   if (p.Slot_NoDH != 0 && p.Posn_NoDH == 0) { msg = $"{p.PlayerType} has slot but no position"; break; }
-            //   if (p.Slot_NoDH == 0 && p.Posn_NoDH != 0) { msg = $"{p.PlayerType} has position but no slot"; break; }
-            //   slot[p.Slot_NoDH]++;
-            //   posn[p.Posn_DH]++;
+            if (totB + totP < 11) return "Team must have at least 11 players (including 1 pitcher)";
+            if (totB + totP > 25) return "Team can't have more than 25 players";
+            if (totB < 10) return "Team must have at least 10 position players plus DH's";
+            if (totP == 0) return "Team must have at least one pitcher";
+            if (totP > 11) return "Team can't have more than 11 pitchers";
 
-            //}
+            int[] slots_NoDH = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] posns_NoDH = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] slots_DH =   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            int[] posns_DH =   { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
-            return true;
+            string[] posnNames = { "", "p", "c", "1b", "2b", "3b", "ss", "lf", "cf", "rf", "dh" };
+
+            foreach (var p in Players) {
+               if (p.Slot_NoDH != 0 && p.Posn_NoDH == 0) return $"{p.PlayerName} has slot but no position"; 
+               if (p.Slot_NoDH == 0 && p.Posn_NoDH != 0) return $"{p.PlayerName} has position but no slot";
+               if (p.Posn_DH == 1 && p.Slot_DH > 0) return $"Pitcher, {p.PlayerName}, should not have position";
+              
+               slots_NoDH[p.Slot_NoDH]++;
+               posns_NoDH[p.Posn_NoDH]++;
+               slots_DH[p.Slot_DH]++;
+               posns_DH[p.Posn_DH]++;
+            }
+
+            for (int i = 1; i < 9; i++) {
+               if (slots_NoDH[i] == 0) return $"No batter in slot {i} in No-DH lineup";
+               if (slots_DH[i] == 0) return $"No batter in slot {i} in DH lineup";
+               if (slots_NoDH[i] > 1) return $"{slots_NoDH[i]} batters in slot {i} in No-DH lineup";
+               if (slots_DH[i] > 1) return $"{slots_DH[i]} batters in slot {i} in No-DH lineup";
+            }
+
+            for (int i = 0; i <= 9; i++) {
+               if (posns_NoDH[i] == 0) return $"No player at position: {posnNames[i]}, in No-DH lineup";
+               if (posns_NoDH[i] > 1) return $"{posns_NoDH[i]} players at position {posnNames[i]}, in No-DH lineup";
+            }
+            for (int i = 0; i <= 10; i++) {
+               if (posns_DH[i] == 0) return $"No player at position{posnNames[i]}, in DH lineup";
+               if (posns_DH[i] > 1) return $"{posns_DH[i]} players at position {posnNames[i]}, in DH lineup";
+            }
+            return "";
 
          }
       }
